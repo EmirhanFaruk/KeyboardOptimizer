87f12958b3a99402f6047043b668e2c45d8abbce
yum <xo140597@gmail.com>
2025-01-01 18:51:30 +0100

    on a bien un nombre inferieur apres l'optimisation avec juste un nb random de changement de touche

--- src/main/java/controller/Main.java
+++ src/main/java/controller/Main.java
@@ -37,8 +37,7 @@ public class Main
 
         System.out.println( "Le score du clavier avant l'optimisation est de " + keyboardEvaluator.evaluateKeyboard(NGrammes) ) ;
 
-        keyboardOptimizer.optimize(NGrammes);
-        keyboard = keyboardOptimizer.getKeyboard() ;
+        keyboard = keyboardOptimizer.optimize(NGrammes);
 
         JSONWriter.saveOptimizedKeyboardAsJSON( keyboard, "optimized_keyboard.json");
         keyboardEvaluator.setKeyboard(keyboard);
--- src/main/java/model/keyboard/KeyboardOptimizer.java
+++ src/main/java/model/keyboard/KeyboardOptimizer.java
@@ -7,10 +7,16 @@ import java.util.*;
 
 public class KeyboardOptimizer {
 
-    private Keyboard keyboard;
+    private final Keyboard keyboardOriginal;
 
-    public KeyboardOptimizer(Keyboard keyboard) {
-        this.keyboard = keyboard;
+    private KeyboardEvaluator keyboardEvaluator ;
+    private Random random;
+    private List<Keyboard> pool;
+    public KeyboardOptimizer( Keyboard keyboard ) {
+        this.keyboardOriginal = keyboard;
+        this.keyboardEvaluator = new KeyboardEvaluator( this.keyboardOriginal ) ;
+        this.random = new Random() ;
+        this.pool = initialisePool() ;
     }
 
     /**
@@ -18,9 +24,17 @@ public class KeyboardOptimizer {
      * @param frequencies Les fréquences d'utilisation des touches.
      * @return Un clavier optimisé.
      */
-    public Keyboard optimize(Map<String[], Integer> frequencies) {
-
-        return keyboard;
+    public Keyboard optimize ( Map<String[], Integer> frequencies ) {
+        Keyboard res =  this.keyboardOriginal ;
+        double bestScore = this.keyboardEvaluator.evaluateKeyboard(frequencies) ;
+        for ( Keyboard keyboard : this.pool ) {
+            this.keyboardEvaluator.setKeyboard(keyboard);
+            double keyboardScore = this.keyboardEvaluator.evaluateKeyboard(frequencies) ;
+            if ( bestScore > keyboardScore ) {
+                res = keyboard ;
+            }
+        }
+        return res ;
     }
 
 
@@ -29,29 +43,63 @@ public class KeyboardOptimizer {
      * @param k1 touche 1
      * @param k2 touche 2
      */
-    public void switchKey(Key k1, Key k2) {
-        ArrayList<ArrayList<Key>> res = new ArrayList<>();
+    public void switchKey ( Keyboard keyboard , Key k1 , Key k2 ) {
         int rowk1 = k1.getRangee(), colk1 = k1.getColumn();
         int rowk2 = k2.getRangee(), colk2 = k2.getColumn();
 
         for (ArrayList<Key> list : keyboard.getKeys()) {
-            ArrayList<Key> temp = new ArrayList<>();
-            for (Key key : list) {
+            for (int i = 0; i < list.size(); i++) {
+                Key key = list.get(i);
+
                 if (key.getRangee() == rowk1 && key.getColumn() == colk1) {
-                    temp.add(new Key(key.getTouchName(), k2.getRangee(), k2.getColumn(), k2.getFinger(), k1.isRightHand(), k1.isShifted(), k1.isAltGr()));
-                } else if (key.getRangee() == rowk2 && key.getColumn() == colk2) {
-                    temp.add(new Key(key.getTouchName(), k1.getRangee(), k1.getColumn(), k1.getFinger(), k2.isRightHand(), k2.isShifted(), k2.isAltGr()));
-                } else {
-                    // Pas de changement pour les autres touches
-                    temp.add(key);
+                    Key newKey = new Key(key.getTouchName(), rowk2, colk2, k2.getFinger(), key.isRightHand(), key.isShifted(), key.isAltGr());
+                    list.set(i, newKey);
+                }
+                else if (key.getRangee() == rowk2 && key.getColumn() == colk2) {
+                    Key newKey = new Key(key.getTouchName(), rowk1, colk1, k1.getFinger(), key.isRightHand(), key.isShifted(), key.isAltGr());
+                    list.set(i, newKey);
                 }
             }
-            res.add(temp);
         }
-        keyboard = new Keyboard(res);
     }
 
-    public Keyboard getKeyboard() {
-        return keyboard;
+    public ArrayList<Keyboard> initialisePool () {
+        ArrayList<Keyboard> res = new ArrayList<>() ;
+        Keyboard kb1 = randomSwitchkey() ;
+        Keyboard kb2 = randomSwitchkey() ;
+        Keyboard kb3 = cross( kb1 , kb2 ) ;
+        Keyboard kb4 = cross( kb2 , kb1 ) ;
+        res.add( kb1 ) ;
+        res.add( kb2 ) ;
+        res.add( kb3 ) ;
+        res.add( kb4 ) ;
+
+        return res ;
+    }
+
+    /**
+     * Une fonction qui est un clavier random
+     * @return clavier random
+     */
+    private Keyboard randomSwitchkey () {
+        Keyboard res = keyboardOriginal.clone() ;
+        int length = res.getKeys().get(0).size() ;
+        int nbChangement = random.nextInt( length ) ;
+        for ( int i = 0 ; i < nbChangement ; i++ ) {
+            Key k1 = res.getKeys().get(0).get( random.nextInt(length) ) ;
+            Key k2 = res.getKeys().get(0).get( random.nextInt(length) ) ;
+            switchKey( res , k1 , k2 );
+        }
+        return res ;
+    }
+
+    /**
+     * Une fonction qui fait la combinaison de deux claviers
+     * @param kb1 premier clavier
+     * @param kb2 deuxième clavier
+     * @return le clavier combiné
+     */
+    private Keyboard cross(Keyboard kb1, Keyboard kb2) {
+        return kb1 ;
     }
 }
